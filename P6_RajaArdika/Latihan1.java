ef3421a534b9466e47981cfaa09ed655412326c9
Raja Ardika <rajaardika09@gmail.com>
2026-04-30 15:43:49 +0700

    Reupload Pertemuan 6

---
+++ P6_RajaArdika/Latihan1.java
@@ -0,0 +1,66 @@
+// Implementasi Class, Object, dan Method
+
+public class Latihan1 {
+
+    private String nama;
+    private double nilaiMath;
+    private double nilaiEnglish;
+    private double nilaiScience;
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setNilaiMath(double nilaiMath) {
+        this.nilaiMath = nilaiMath;
+    }
+
+    public void setNilaiEnglish(double nilaiEnglish) {
+        this.nilaiEnglish = nilaiEnglish;
+    }
+
+    public void setNilaiScience(double nilaiScience) {
+        this.nilaiScience = nilaiScience;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public double getAverage() {
+        double result = (nilaiMath + nilaiEnglish + nilaiScience) / 3;
+        return result;
+    }
+
+    public String getKeterangan() {
+        double avg = getAverage();
+
+        if (avg >= 85) {
+            return "Sangat Baik";
+        } else if (avg >= 70) {
+            return "Baik";
+        } else if (avg >= 60) {
+            return "Cukup";
+        } else {
+            return "Kurang";
+        }
+    }
+
+    public void tampilData() {
+        System.out.println("Nama: " + getNama());
+        System.out.println("Rata-rata: " + getAverage());
+        System.out.println("Keterangan: " + getKeterangan());
+    }
+
+    public static void main(String[] args) {
+
+        Latihan1 siswa = new Latihan1();
+
+        siswa.setNama("Raja Ardika");
+        siswa.setNilaiMath(80);
+        siswa.setNilaiEnglish(90);
+        siswa.setNilaiScience(85);
+
+        siswa.tampilData();
+    }
+}
---
+++ P6_RajaArdika/Latihan2.java
@@ -0,0 +1,50 @@
+//Implementasi Overloading Method
+
+public class Latihan2 {
+
+    private String nama;
+    private String universitas;
+    private String fakultas;
+
+    // setter
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public void setUniversitas(String universitas) {
+        this.universitas = universitas;
+    }
+
+    public void setFakultas(String fakultas) {
+        this.fakultas = fakultas;
+    }
+
+    // OVERLOADING METHOD
+    public void tampil(String text) {
+        System.out.println("Nama: " + nama);
+        System.out.println("Universitas: " + universitas);
+        System.out.println("Fakultas: " + fakultas);
+    }
+
+    public void tampil(int pilihan) {
+        System.out.println("Nama: " + nama);
+        System.out.println("Fakultas: " + fakultas);
+    }
+
+    public static void main(String[] args) {
+
+        Latihan2 obj = new Latihan2();
+
+        obj.setNama("Raja Ardika");
+        obj.setUniversitas("Universitas Pamulang");
+        obj.setFakultas("Ilmu Komputer");
+
+        System.out.println("=== Method 1 ===");
+        obj.tampil("Data Lengkap");
+
+        System.out.println();
+
+        System.out.println("=== Method 2 ===");
+        obj.tampil(1);
+    }
+}
