16b20f33f87f15e9fa5395cef640dd16aba9e361
Raja Ardika <rajaardika09@gmail.com>
2026-04-30 12:59:44 +0700

    Reupload Pertemuan 3

---
+++ P3_RajaArdika/Latihan2.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class Latihan2 {
+    public static void main(String[] args) {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            System.out.print("Masukkan nama: ");
+            String nama = input.readLine();
+
+            System.out.print("Masukkan umur: ");
+            int umur = Integer.parseInt(input.readLine());
+
+            System.out.println("\n=== Data Anda ===");
+            System.out.println("Nama: " + nama);
+            System.out.println("Umur: " + umur);
+
+        } catch (IOException e) {
+            System.out.println("Terjadi kesalahan input!");
+        } catch (NumberFormatException e) {
+            System.out.println("Umur harus angka!");
+        }
+    }
+}
+
